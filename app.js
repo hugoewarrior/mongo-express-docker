6dbdaabc15f46cdc4b10d8c4f6cb929b180a44b7
root <root@vultr.guest>
2020-07-31 05:08:37 +0000

    Lab 6 y Lab 7

--- app.js
+++ app.js
@@ -1,30 +1,31 @@
-const Estudiantes = require('./src/estudiantes');
+//const Estudiantes = require('./src/estudiantes');
 const bodyParser = require('body-parser');
-
-//var mongoose = require('mongoose');
+require('dotenv').config();
+var mongoose = require('mongoose');
 const cors = require('cors');
 const express = require('express');
 const app = express();
+
 
 /**
  * DB connection
  */
 
-/* var mongoDB = process.env.MONGODB_SERVER;
+var mongoDB = process.env.MONGODB_SERVER;
 mongoose.connect(mongoDB, { useNewUrlParser: true, });
 var db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.on('open', (m) => console.log("Database connection opened, we are cool to go!"));
 
- */
+
 /**
  * Pug Injectors
  */
 
-/* app.set('view engine', 'pug');
+app.set('view engine', 'pug');
 app.set('views', './views');
- */
+
 
 /**
  * Middlewares 
@@ -40,38 +41,38 @@ app.use(cors());
  * DB Schemas
  */
 
-//const Estudiantes = require('./models/Estudiantes');
+const Estudiantes = require('./models/Estudiantes');
 
 
 /**
  * Start - Services ************************************************************************
  */
 
-const st = new Estudiantes();
+//const st = new Estudiantes();
 
 /**
  * Brings all students
  */
-app.get('/estudiantes', (req, resp) => {
+/* app.get('/estudiantes', (req, resp) => {
     resp.send(JSON.stringify(st.sudents))
 });
+ */
 
-
-/* app.get('/estudiantes', async (req, res) => {
+app.get('/estudiantes', async (req, res) => {
     const estudiantes = await Estudiantes.find().select('nombre edad');
     res.render('estudiantes', { estudiantes });
-}); */
+});
 
 /**
  * Brings specific student
  */
 
-app.get('/estudiantes/:id', (req, resp) => {
+/* app.get('/estudiantes/:id', (req, resp) => {
     let ans = st.getSpecificStudent(req.params.id)
     resp.send(JSON.stringify(ans))
-});
+}); */
 
-/* app.get('/estudiantes/:id', async (req, res) => {
+app.get('/estudiantes/:id', async (req, res) => {
     try {
         const estudiante = await Estudiantes.findById(req.params.id).select('nombre edad');
         res.render('estudiantes_detail', { estudiante });
@@ -79,45 +80,45 @@ app.get('/estudiantes/:id', (req, resp) => {
         console.log(error);
         res.send('Error');
     }
-}); */
+});
 
 /**
  * Creates a new student
  */
 
-app.post('/estudiantes', (req, resp) => {
+/* app.post('/estudiantes', (req, resp) => {
     let ans = st.createStudent(req.body)
     resp.send(ans)
-});
+}); */
 
-/* app.post('/estudiantes', async (req, res) => {
+app.post('/estudiantes', async (req, res) => {
     const { nombre, edad } = req.body;
     await Estudiantes.create({ nombre, edad });
     const estudiantes = await Estudiantes.find().select('nombre edad');
     res.render('estudiantes', { estudiantes, success: true });
-}); */
+});
 
 /**
  * Edits one student
  */
-app.put('/estudiantes', (req, resp) => {
+/* app.put('/estudiantes', (req, resp) => {
     let ans = st.editStudent(req.body)
     resp.send(ans)
 });
-
+ */
 
 
 /**
  * Deletes one student
  */
 
-app.delete('/estudiantes/:id', (req, resp) => {
+/* app.delete('/estudiantes/:id', (req, resp) => {
     let ans = st.deleteStudent(req.params.id)
     resp.send(JSON.stringify(ans))
 });
+ */
 
-
-/* app.delete('/api/estudiantes/:id', async (req, res) => {
+app.delete('/api/estudiantes/:id', async (req, res) => {
     try {
         await Estudiantes.findByIdAndDelete(req.params.id);
         res.status(202).json({});
@@ -125,7 +126,7 @@ app.delete('/estudiantes/:id', (req, resp) => {
         console.log(error);
         res.json({});
     }
-}); */
+});
 
 /**
  * End - Services ***************************************************
